071f5a087dccd2016ac72882a544739cb9df986d
nancy77zion <nancy77zion@gmail.com>
2023-07-21 03:48:00 +0100

    created new branch

--- src/components/SuccessPage.jsx
+++ src/components/SuccessPage.jsx
@@ -3,10 +3,12 @@
 const SuccessPage = ({ data }) => {
   return (
     <div>
+      <h1>Thank you for shopping</h1>
       <h2 className='font-extrabold'>Order Successful!</h2>
       <p>Name: {data.name}</p>
       <p>Email: {data.email}</p>
       <p>Address: {data.address}</p>
+
     </div>
   );
 };
